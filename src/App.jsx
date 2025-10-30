901d96f5632b8dabca0d172506b3d1860b513221
Tanish <tanishmmb@gamail.com>
2025-10-30 19:40:15 +0530

    Initial Phase 1 setup with Vite, React, Tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AppRoutes from "./routes";
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <AppRoutes />
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+export default function HomePage() {
+  return (
+    <h1 className="text-4xl font-bold text-red-500">TailwindCSS v4 is Working!</h1>
+  )
+}
